e826f782ab6425471c614b35a2ac91deaf48ea0a
sahaynitin <85211814+sahaynitin@users.noreply.github.com>
2021-11-05 21:23:37 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -56,4 +56,11 @@ class Config(object):
 
     # Sql Database url
     DB_URI = os.environ.get("DATABASE_URL", "")
-    
+    DOWNLOAD_DIR = os.environ.get("DOWNLOAD_DIR", "./downloads")
+    LOGGER = logging
+    OWNER_ID = int(os.environ.get("OWNER_ID", 1445283714))
+    PRO_USERS = list(set(int(x) for x in os.environ.get("PRO_USERS", "0").split()))
+    PRO_USERS.append(OWNER_ID)
+    MONGODB_URI = os.environ.get("MONGODB_URI", "")
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-100"))
+    BROADCAST_AS_COPY = bool(os.environ.get("BROADCAST_AS_COPY", "False"))
